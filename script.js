f23e41785d90621c271b40db8c340507c77b1d6e
Maja <56198907+MajaA95@users.noreply.github.com>
2022-12-04 20:55:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+//50 MPG
+
+var oneKmLToMpg = 2.3521458
+var mpg = 50
+var l100Km = mpg / oneKmLToMpg
+
+console.log(l100Km)
+
+//25 MPG
+var oneKmLToMpg =2.3521458
+var mpg= 25
+var l100Km = mpg / oneKmLToMpg
+
+console.log (l100Km)
+
+//12L/100km
+
+var oneKmLToMpg = 2.3521458
+var l100Km = 12
+var mpg = l100Km * oneKmLToMpg
+
+console.log (mpg)
+
+
+
+
+
